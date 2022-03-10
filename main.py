70be7cee7cb78bac14bc14390ae6a59cc81c58b5
szendrattila <71691690+szendrattila@users.noreply.github.com>
2022-03-10 14:57:20 +0000

    gyakorlas0310

--- main.py
+++ main.py
@@ -178,6 +178,7 @@ for kulcs, ertek in stat.items():
 ###########################################################################
 #Ausztria;1995.01.01
 #Belgium;1958.01.01
+"""
 class Eu:
     def __init__(self, sor):
         orszag, datum = sor.strip().split(";")
@@ -215,3 +216,43 @@ for sor in lista:
     stat[sor.ev] = stat.get(sor.ev, 0) + 1
 for kulcs, ertek in stat.items():
     print(f"        {kulcs} - {ertek} ország")
+"""
+###########################################################################
+#Ausztria;1995.01.01
+#Belgium;1958.01.01
+class Eu:
+    def __init__(self, sor):
+        orszag, datum = sor.strip().split(";")
+        self.orszag = orszag
+        self.datum = datum
+        self.ev = int(datum[:4])
+        self.honap = int(datum[5:7])
+        self.nap = int(datum[8:10])
+lista = []
+with open("EUcsatlakozas.txt", encoding="latin2") as f:
+    for sor in f:
+        lista.append(Eu(sor))
+#3.feladat
+print(f"3.feladat: Eu tagállamainak száma: {len(lista)} db")
+#4.feladat
+csatlakozas = len([sor for sor in lista if sor.ev == 2007])
+print(f"4.feladat: 2007-ben {csatlakozas} ország csatlakozott")
+#5.feladat
+magyar = [sor.datum for sor in lista if sor.orszag == "Magyarország"]
+print(f"5.feladat: Magyarország csatlakozásának dátuma: {magyar[0]}")
+#6.feladat
+volte = len([sor for sor in lista if sor.honap == 5])
+if volte > 0:
+    print(f"6.feladat: Májusban volt csatlakozás!")
+else:
+    print(f"6.feladat: Májusban volt csatlakozás!")
+#7.feladat
+utoljara = max(lista, key=lambda x:x.datum).orszag
+print(f"7.feladat legutoljára csatlakozott ország: {utoljara}")
+#8.feladat
+print("8.feladat")
+stat = dict()
+for sor in lista:
+    stat[sor.ev] = stat.get(sor.ev, 0) + 1
+for kulcs, ertek in stat.items():
+    print(f"       {kulcs} - {ertek} ország")
